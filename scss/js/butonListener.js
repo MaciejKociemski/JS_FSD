d5e2cf00fc418e2f9f21523e81c5950bdd91a2c4
Maciej Kociemski <maciej.kociemski@gmail.com>
2023-03-08 15:43:49 +0100

    added

--- scss/js/butonListener.js
+++ scss/js/butonListener.js
@@ -2,5 +2,5 @@ console.log()
 
 const button = document.getElementById("button");
 button.addEventListener('click', function () {
-    console.log("klik sie odbył, kliknięto przycisk")
+    console.log("kliknięto przycisk")
 })
