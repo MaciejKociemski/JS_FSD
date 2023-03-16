a91d9481081e4f069c22ae38b0cb329860422e38
Maciej Kociemski <maciej.kociemski@gmail.com>
2023-03-16 20:27:40 +0100

    feat:add code from modul 20, object countries construction

---
+++ scss/js/countriesObject.js
@@ -0,0 +1,18 @@
+countries = [
+ { 
+id: 1, 
+name: { official: "Poland" }, 
+capital: "Warsaw",
+population: 38_036_118,
+flags: { svg: "https://en.wikipedia.org/wiki/Poland#/media/File:Flag_of_Poland.svg" },
+languages: ["Polish"],
+},
+{ 
+id: 2, 
+name: { official: "Republic of Estonia" }, 
+capital: "Tallinn",
+population: 1_357_739,
+flags: { svg: "https://en.wikipedia.org/wiki/Estonia#/media/File:Flag_of_Estonia.svg" },
+languages: ["Estonian"],
+},
+]
