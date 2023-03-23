fd0772ef3d96657f2beb3fbf6abdf1b7b59c3495
Maciej Kociemski <maciej.kociemski@gmail.com>
2023-03-23 20:49:10 +0100

    feat added info about functional programming

---
+++ scss/js/functionalProgramming.js
@@ -0,0 +1,8 @@
+// PURE FUNCTION
+// a) SAME INPUT === SAME OUTPUT (dla tego samego inputu ten sam output)
+// b) NO SIDE EFFECTS (brak efektów ubocznych)
+// 2. IMMUTABILITY
+// 3. HOF (HIGHER-ORDER FUNCTION) - funkcja wyższego rzędu 
+// INPUT OR/AND OUTPUT -> FUNCTION
+// INPUT FUNCTION === CALLBACK [1, 2, 3].map((n) => n);
+// OUTPUT FUNCTION === CLOSURE (fn) =>  fn;
